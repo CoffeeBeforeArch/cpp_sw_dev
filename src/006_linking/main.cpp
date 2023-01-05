5c37bcfcf734019a5cf3a905dc34c7c9727a3883
Nick <coffeebeforearch@gmail.com>
2023-01-05 09:50:24 -0600

    Add linking example

---
+++ src/006_linking/main.cpp
@@ -0,0 +1,9 @@
+// A simple program for showing off static/dynamic linking
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!\n";
+    return 0;
+}
