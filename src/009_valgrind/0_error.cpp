1abb5708db6aa4ba0881fcd923a95cce4563d575
Nick <coffeebeforearch@gmail.com>
2023-02-19 17:25:30 -0600

    Formatting

--- src/009_valgrind/0_error.cpp
+++ src/009_valgrind/0_error.cpp
@@ -6,13 +6,13 @@
 #include <iostream>
 
 int main() {
-    // Create an array of 5 integers
-    std::array<int, 5> my_array = {42, 3, 39, 4, 9};
-    
-    // A for loop with an off-by-one error
-    for(auto i = 0u; i <= my_array.size(); i++) {
-        std::cout << my_array[i] << '\n';
-    }
+  // Create an array of 5 integers
+  std::array<int, 5> my_array = {42, 3, 39, 4, 9};
 
-    return 0;
+  // A for loop with an off-by-one error
+  for (auto i = 0u; i <= my_array.size(); i++) {
+    std::cout << my_array[i] << '\n';
+  }
+
+  return 0;
 }
--- src/009_valgrind/3_error.cpp
+++ src/009_valgrind/3_error.cpp
@@ -6,31 +6,31 @@
 #include <random>
 
 int main() {
-    // Create some arrays
-    int N = 1 << 10;
-    float *a = new float[N];
-    float *b = new float[N];
+  // Create some arrays
+  int N = 1 << 10;
+  float *a = new float[N];
+  float *b = new float[N];
 
-    // Create a random number generator
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution dist(0.0f, 1.0f);
+  // Create a random number generator
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_real_distribution dist(0.0f, 1.0f);
 
-    // Generate some random numbers
-    std::generate(a, a + N, [&]{return dist(mt);});
-    std::generate(b, b + N, [&]{return dist(mt);});
+  // Generate some random numbers
+  std::generate(a, a + N, [&] { return dist(mt); });
+  std::generate(b, b + N, [&] { return dist(mt); });
 
-    // Do vector addition
-    for(int i = 0; i < N; i ++) {
-        a[i] = a[i] + b[i];
-    }
+  // Do vector addition
+  for (int i = 0; i < N; i++) {
+    a[i] = a[i] + b[i];
+  }
 
-    // Free our memory
-    delete [] a;
-    delete [] b;
+  // Free our memory
+  delete[] a;
+  delete[] b;
 
-    // Read memory after it has been freed
-    std::cout << a[0] << '\n';
-    std::cout << b[0] << '\n';
-    return 0;
+  // Read memory after it has been freed
+  std::cout << a[0] << '\n';
+  std::cout << b[0] << '\n';
+  return 0;
 }
