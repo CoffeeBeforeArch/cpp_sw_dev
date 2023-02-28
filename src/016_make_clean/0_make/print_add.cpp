a39b8be5bac15efbd1ac140cf8e6f34097cab6f1
Nick <coffeebeforearch@gmail.com>
2023-02-28 09:24:25 -0600

    Add make clean example

---
+++ src/016_make_clean/0_make/print_add.cpp
@@ -0,0 +1,10 @@
+// A simple program that prints the sum of two numbers
+// By: Nick from CoffeeBeforeArch
+
+#include "add.h"
+#include <iostream>
+
+int main() {
+  std::cout << add(10, 20) << '\n';
+  return 0;
+}
