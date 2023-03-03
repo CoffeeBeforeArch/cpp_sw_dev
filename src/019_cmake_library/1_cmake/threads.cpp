1c15e2bfef27cd7f7da7e409e29d9c065d87a9a2
Nick <coffeebeforearch@gmail.com>
2023-03-03 10:05:43 -0600

    formatting

--- src/019_cmake_library/1_cmake/threads.cpp
+++ src/019_cmake_library/1_cmake/threads.cpp
@@ -1,34 +1,33 @@
 // A simple example where we link against threads
 // By: Nick from CoffeeBeforeArch
 
+#include <atomic>
 #include <iostream>
 #include <thread>
-#include <atomic>
 
 int main() {
-    // Number of increments per-thread
-    const int num_increments = 1 << 20;
+  // Number of increments per-thread
+  const int num_increments = 1 << 20;
 
-    // Work lambda with protected increments
-    int val = 0;
-    std::mutex m;
-    auto work = [&]{
-        for(int i = 0; i < num_increments; i++) {
-            std::lock_guard<std::mutex> lg(m);
-            val += 1;
-            }
+  // Work lambda with protected increments
+  int val = 0;
+  std::mutex m;
+  auto work = [&] {
+    for (int i = 0; i < num_increments; i++) {
+      std::lock_guard<std::mutex> lg(m);
+      val += 1;
+    }
+  };
 
-    };
+  // Spawn our threads
+  std::thread t0(work);
+  std::thread t1(work);
 
-    // Spawn our threads
-    std::thread t0(work);
-    std::thread t1(work);
-    
-    // Wait for threads to finish
-    t0.join();
-    t1.join();
+  // Wait for threads to finish
+  t0.join();
+  t1.join();
 
-    // Print the final result
-    std::cout << "Final Value: " << val << '\n';
-    return 0;
+  // Print the final result
+  std::cout << "Final Value: " << val << '\n';
+  return 0;
 }
